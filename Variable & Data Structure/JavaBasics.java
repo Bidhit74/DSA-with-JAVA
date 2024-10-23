2c183c7e23201c89e24537ec3a198a7cd633c3bc
Bidhit <bidhitkumar61@gmail.com>
2024-10-23 07:26:50 +0530

    Java Basics

---
+++ Variable & Data Structure/JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics{
+    public static void main(String args[]) {
+        // System.out.print("Hello, Java ");
+        // System.out.print("Bidhit\n"); // next line
+        // or
+        // System.out.println("Bidhit Chaudhary"); // next line
+
+        // Variables
+        int a = 5;
+        int b = 10;
+        String name = "Bidhit Chaudhary";
+        System.out.println(2+(a+b));
+        System.out.println(name);
+    }
+}
---
+++ Variable & Data Structure/README.md
@@ -0,0 +1,10 @@
+# Java Basics
+
+-   System.out.print() - prints without adding a new line.
+-   System.out.println() - prints and moves to the next line.
+
+# Variables
+
+-   5+(a+b)
+-   This is 5 is literal or constant jo universal hota hai change nahi hota hai
+-   "a+b" is a variable jiski value change ho sakti hai
