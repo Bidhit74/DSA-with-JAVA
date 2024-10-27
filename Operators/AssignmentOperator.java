eb24cd8f239de7a7cd47c6d209d9d6a0ebcfb9ac
Bidhit <bidhitkumar61@gmail.com>
2024-10-27 20:53:57 +0530

    Assignment Operators

---
+++ Operators/AssignmentOperator.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        // Assignment Operators: =, +=, -=, *=, /=, %= to assign values.
+        int a = 5;
+        a += 5; // a + 5 = 10
+        a -= 3; // a - 3 = 7 
+        a *= 3; // a * 3 = 21
+        a /= 3; // a / 3 = 7
+        a %= 4; // a % 4 = 3 (Remainder)
+        System.out.println(a);
+    }
+}
