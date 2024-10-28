77c361703f9af9786d351647c71fb447d0a67bd2
Bidhit <bidhitkumar61@gmail.com>
2024-10-28 09:31:59 +0530

    if else conditon

---
+++ Conditional Statements/ifClass.java
@@ -0,0 +1,35 @@
+public class ifClass {
+    public static void main(String[] args) {
+        
+        //Print if a number is Odd or Even
+        int n = 7;
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+        
+        //Print the largest of 2 numbers
+        /* 
+        int a = 20;
+        int b = 20;
+        if (a > b) {
+            System.out.println(a);
+        }
+        else {
+            System.out.println(b);
+        }
+        */
+        
+        /* 
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Yes, You can drive.");
+        }
+        else {
+            System.out.println("You can not drive.");
+        }
+        */
+    }
+}
