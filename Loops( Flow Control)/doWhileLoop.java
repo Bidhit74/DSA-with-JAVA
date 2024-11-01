b9fe465f91cbf9ebafb843efe6da4df889c782ad
Bidhit <bidhitkumar61@gmail.com>
2024-11-01 15:44:55 +0530

    do-while Loop

---
+++ Loops( Flow Control)/doWhileLoop.java
@@ -0,0 +1,11 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        // do-while Loop  -   Similar to while, but executes code at least once before checking the condition.
+        int count = 1;
+        do{
+            System.out.println("Hello Bidhi!  " + count);
+            count++;
+        }
+        while(count<=10);
+    }
+}
