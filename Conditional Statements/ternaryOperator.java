5d4adb4a625a94bc3745907e12321279b98367dd
Bidhit <bidhitkumar61@gmail.com>
2024-11-01 13:41:56 +0530

    Ternary Operator

---
+++ Conditional Statements/ternaryOperator.java
@@ -0,0 +1,7 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int n = 6;
+        String type = (n % 2 == 0) ? "Even" : "Odd";
+        System.out.println(type);
+    }
+}
