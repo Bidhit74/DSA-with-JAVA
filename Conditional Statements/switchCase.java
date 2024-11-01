925bc960c84281d0fd407a9bb931677085a11602
Bidhit <bidhitkumar61@gmail.com>
2024-11-01 14:03:57 +0530

    Switch Statements

---
+++ Conditional Statements/switchCase.java
@@ -0,0 +1,15 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int n = 4;
+
+        switch (n) {
+            case 1: System.out.println("Hello 1");
+            break;
+            case 2: System.out.println("Hello 2");
+            break;
+            case 3: System.out.println("Hello 3");
+            break;
+            default: System.out.println("Hello Default");
+        }
+    }
+}
