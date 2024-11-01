13d635cf751078d0f0123cb1ec4cf17c6f25fac0
Bidhit <bidhitkumar61@gmail.com>
2024-11-01 14:57:00 +0530

    SquarePattern

---
+++ Loops( Flow Control)/SquarePattern.java
@@ -0,0 +1,16 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        // SquarePattern
+        // * * * *
+        // * * * *
+        // * * * *
+        // * * * *
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
