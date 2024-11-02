1741995cdacc8f3dde437c40c89c9984d42a27fe
Bidhit <bidhitkumar61@gmail.com>
2024-11-02 08:39:48 +0530

    print star pattern - triangle pattern

---
+++ Patterns( Part - 1)/StarPattern1.java
@@ -0,0 +1,19 @@
+public class StarPattern1 {
+    public static void main(String[] args) {
+        //print star pattern - triangle pattern
+        /*
+            *
+            * *
+            * * *
+            * * * *
+            * * * * *
+         */
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
