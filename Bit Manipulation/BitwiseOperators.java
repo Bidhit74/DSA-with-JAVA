ad4320dc1f555c386d80cedf25753e4fff9dd436
Bidhit <bidhitkumar61@gmail.com>
2024-11-28 19:57:58 +0530

    Binary Leff Shift (<<)

--- Bit Manipulation/BitwiseOperators.java
+++ Bit Manipulation/BitwiseOperators.java
@@ -28,19 +28,26 @@ public class BitwiseOperators {
     // ~0 = 1
     // Binary One's complement (~) or Not Operator Question
     // Two term in this operator 
-    //LSB(List Significat Bit) --> (1)
+    //LSB(Least Significat Bit) --> (1)
     //MSB(Most Significat Bit) --> (0)
     // Tow's Complement --> First you need to one's complement than add 1. 
     // ~5 --> ~(0000...101) = 1111...010  --> number Negation (MSB = 1(Negative)) 
     //Two's complement
     // 1111...010 --> one's complement --> 0000...101 + 1 --> 0000...110 --> (6) But MSB(Negative) than answer is (-6)
 
-    
+    // Binary Leff Shift (<<)
+    // Q :- 5 << 2   00000…101 Left shift (<<)  = 000..10100    16
+	// Formula :-   a  <<  b  =  a *  2b 
+	// 5 << 2  =  5 * 22  = 5 * 4 = 20 
+
+
     public static void main(String[] args) {
         System.out.println(5 & 6); // Output is 4.
         System.out.println(5 | 6); // Output is 7.
         System.out.println(5 ^ 6); // Output is 3.
         System.out.println(~5); // Output is -6.
         System.out.println(~0); // Output is -6.
+        System.out.println(5 << 2); // Output is 20.
+        System.out.println(5 << 5); // Output is 160.
     }
 }
