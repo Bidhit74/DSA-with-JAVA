b7610ceca15d0333df11ea46d2d4e07b9be97838
Bidhit <bidhitkumar61@gmail.com>
2024-11-28 18:44:45 +0530

    Binary OR(|)

--- Bit Manipulation/BitwiseOperators.java
+++ Bit Manipulation/BitwiseOperators.java
@@ -8,7 +8,18 @@ public class BitwiseOperators {
     // Binary AND operator Question
     // 5 & 6 --> 101 & 110 = 100 --> (4)
 
+
+    // Binary OR(|)
+    // 1 | 1 = 1
+    // 1 | 0 = 1
+    // 0 | 1 = 1
+    // 0 | 0 = 0
+
+    // Binary OR operator Question
+    // 5 | 6 --> 101 | 110 = 111 --> (7)
+
     public static void main(String[] args) {
-        System.out.println(5 & 6);
+        System.out.println(5 & 6); // Output is 4.
+        System.out.println(5 | 6); // Output is 7.
     }
 }
