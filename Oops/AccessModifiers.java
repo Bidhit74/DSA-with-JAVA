6d8d471484e035e7cf44a1a27666bf628a8b936a
Bidhit <bidhitkumar61@gmail.com>
2024-12-03 20:48:02 +0530

    Types of Access Modifiers

---
+++ Oops/AccessModifiers.java
@@ -0,0 +1,19 @@
+public class AccessModifiers {
+    public static void main(String[] args) {
+        BankAccount b = new BankAccount();
+        b.username = "Bidhit";
+        // b.password = "Bidhit123@@"; // Not set kuyki password privet specifier
+        b.setPassword("Bidhit123@@"); // But yah function password ko set kar sakta kuyki yah within the class use hua hai
+        System.out.println(b.username);
+        // System.out.println(b.password); // password has private access in BankAccount
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String psw) {
+        password = psw;
+    }
+}
---
+++ Oops/readme.md
@@ -0,0 +1,7 @@
+# Types of Access Modifiers:
+
+-   Modifier -- Class -- Package -- Subclass -- World (Other Packages)
+-   public -- ✔ -- ✔ -- ✔ -- ✔
+-   protected -- ✔ -- ✔ -- ✔ -- ✘ (only via inheritance)
+-   default -- ✔ -- ✔ -- ✘ -- -- ✘
+-   private -- ✔ -- ✘ -- ✘ -- ✘
