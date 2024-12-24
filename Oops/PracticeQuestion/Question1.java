16aa499ec47205283380a66257d652e41017226d
Bidhit <bidhitkumar61@gmail.com>
2024-12-24 12:23:54 +0530

    Question 1

---
+++ Oops/PracticeQuestion/Question1.java
@@ -0,0 +1,14 @@
+// Find out the correct statement to assign name to object.
+
+class Student {
+    String name;
+    int marks;
+}
+
+public class Question1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Bidhit";
+        System.out.println(s.name);
+    }
+}
