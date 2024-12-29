19a4a0bf6ec93f86b3559e61274e1f25100ec029
Bidhit <bidhitkumar61@gmail.com>
2024-12-29 20:14:47 +0530

    Print X^N (X to the power of N) using recursion

---
+++ Recursion/PrintXPowerN.java
@@ -0,0 +1,19 @@
+// Print X^N (X to the power of N) using recursion
+
+public class PrintXPowerN {
+    public static int XPowerN(int x, int n) {
+        // Base case: x^0 = 1
+    if (n == 0) {
+        return 1;
+    }
+    // If n is negative, handle it by taking reciprocal of x
+    if (n < 0) {
+        x = 1 / x;
+        n = -n;
+    }
+        return x * XPowerN(x, n - 1);
+    }
+    public static void main(String[] args) {
+        System.out.println(XPowerN(5, 5));
+    }
+}
