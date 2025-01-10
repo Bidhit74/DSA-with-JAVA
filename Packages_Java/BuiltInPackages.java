5b296e95a44ff26d9027c32c7ef43329aa351200
Bidhit <bidhitkumar61@gmail.com>
2025-01-10 21:39:12 +0530

    Built In Packages

---
+++ Packages_Java/BuiltInPackages.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class BuiltInPackages {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        
+        System.out.println(list); // Output: [Apple, Banana]
+    }
+}
+
+//java.lang:
+// Contains fundamental classes like String, Math, Object.
+
+//java.util:
+// Includes utility classes like ArrayList, HashMap, Date
+
+//java.net:
+// Includes classes for network operations like URL, Socket.
+
+// java.io:
+// Contains classes for input and output like File, BufferedReader.
+
+// java.sql:
+// Provides classes for database connectivity like Connection, ResultSet.
