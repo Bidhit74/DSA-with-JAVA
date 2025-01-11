eb1690191bd929d9d38553da3bbbb8cc06ad34d5
Bidhit <bidhitkumar61@gmail.com>
2025-01-11 11:33:14 +0530

    ArrayIndexOutOfBoundsException

--- Exception Handling/HandlingException.java
+++ Exception Handling/HandlingException.java
@@ -14,9 +14,17 @@ public class HandlingException {
     public static void main(String[] args) {
         int[] numerator = { 10, 200, 30, 40 };
         int[] denominator = { 2, 0, 5, 4 };
-        for (int i = 0; i < numerator.length; i++) {
-            int result = divide(numerator[i], denominator[i]);
-            System.out.println(result);
+        for (int i = 0; i < 5; i++) {
+            try {
+                int result = divide(numerator[i], denominator[i]);
+                System.out.println("Result: " + result);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: Array index out of bound : " + e);
+            }
+            // Jab upper wala eceptions nahi rahega koi other exceptions rahega tab yah catch block chalega 
+            catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
         }
         System.out.println("Good Job!");
     }
