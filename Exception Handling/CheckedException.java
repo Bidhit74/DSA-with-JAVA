fc8caea71884978aba6bc4120cdfa1e227a06b2c
Bidhit <bidhitkumar61@gmail.com>
2025-01-11 12:32:11 +0530

    Refactor CheckedException class to properly handle FileNotFoundException and demonstrate method chaining

--- Exception Handling/CheckedException.java
+++ Exception Handling/CheckedException.java
@@ -1,9 +1,11 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+// import java.io.IOException;
 
 public class CheckedException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Exception is a checked exception, so we need to handle it.
+        /* 
         try {
             FileReader file = new FileReader("a.text");
         }
@@ -13,5 +15,14 @@ public class CheckedException {
         catch (Exception e) {
             System.out.println("Exception caught in main method :- " + e);
         }
+            */
+            method2();
+    }
+
+    public static void method2() throws FileNotFoundException {
+        method1();
+    }
+    public static void method1() throws FileNotFoundException {
+        FileReader file = new FileReader("a.text");
     }
 }
