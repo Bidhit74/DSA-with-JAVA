8f339fe7a6c4c26f2c4cbefff6cf399f85e8b623
Bidhit <bidhitkumar61@gmail.com>
2025-02-26 17:06:19 +0530

    Refactor MergeSort class for improved readability and modify merge logic for descending order sorting

--- DivideConquer/MergeSort.java
+++ DivideConquer/MergeSort.java
@@ -15,14 +15,16 @@ public class MergeSort {
     // Time Complexity: O(n log n) and Space Complexity: O(n)
     //Depth First Structure Use (DFS)
     public static void mergeSort(int[] arr, int start, int end) {
-        if (start >= end) return; // Base case
-        
+        if (start >= end)
+            return; // Base case
+
         int mid = start + (end - start) / 2;
-        mergeSort(arr, start, mid);   // Sort left half
+        mergeSort(arr, start, mid); // Sort left half
         mergeSort(arr, mid + 1, end); // Sort right half
-        merge(arr, start, mid, end);  // Merge sorted halves
+        merge(arr, start, mid, end); // Merge sorted halves
     }
-/* 
+
+    // Descending order
     public static void merge(int arr[], int sIndx, int mid, int eIndx) {
         int[] temp = new int[eIndx - sIndx + 1];
         int i = sIndx; // Iterator index for left side
@@ -30,7 +32,7 @@ public class MergeSort {
         int k = 0; // Iterator index for temp array
 
         while (i <= mid && j <= eIndx) {
-            if (arr[i] < arr[j]) {
+            if (arr[i] > arr[j]) {
                 temp[k++] = arr[i++];
             } else {
                 temp[k++] = arr[j++];
@@ -49,10 +51,10 @@ public class MergeSort {
             arr[i] = temp[k];
         }
     }
-        */
 
     // or
     // Merge function
+    /*
     public static void merge(int[] arr, int start, int mid, int end) {
         int[] temp = new int[end - start + 1];
         int i = start, j = mid + 1, k = 0;
@@ -63,8 +65,9 @@ public class MergeSort {
         while (j <= end) temp[k++] = arr[j++]; // Copy remaining right part
         System.arraycopy(temp, 0, arr, start, temp.length); // Copy back to original array
     }
+         */
     public static void main(String[] args) {
-        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        int arr[] = { 9,6, 3, 9, 5, 2, 8,20 };
         System.out.print("Unsorted : ");
         printArray(arr);
         mergeSort(arr, 0, arr.length - 1);
