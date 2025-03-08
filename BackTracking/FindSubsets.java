2fb1c8eb204e78d29d6eac9ddb458789e080c93b
Bidhit <bidhitkumar61@gmail.com>
2025-03-08 18:24:15 +0530

    Add FindSubsets class to generate and print all subsets of a given string

---
+++ BackTracking/FindSubsets.java
@@ -0,0 +1,27 @@
+// Find Subsets : find and print all subsets of given string.
+// input = "abc"
+// output = a, b, c, ab, bc, ac, abc
+
+public class FindSubsets {
+
+    public static void findSubset(String str, String ans, int idx) {
+        // Base case:
+        if (idx == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }        
+        //Recursive case:
+        //Yes Choice
+        findSubset(str, ans + str.charAt(idx), idx + 1);
+        //No Choice
+        findSubset(str, ans, idx+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubset(str, "", 0);
+    }
+}
