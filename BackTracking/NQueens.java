fc7b1f59cc221705d9fe76e5fdc91df51a109646
Bidhit <bidhitkumar61@gmail.com>
2025-03-14 20:29:06 +0530

    Now fully solved this problem and no queens attack each other

--- BackTracking/NQueens.java
+++ BackTracking/NQueens.java
@@ -10,12 +10,35 @@ public class NQueens {
         }
         // column wise loop
         for (int j = 0; j < board.length; j++) {
-            board[row][j] = 'Q';
-            nQueens(board, row + 1); // Function call
-            board[row][j] = '.'; // Backtracking step
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1); // Function call
+                board[row][j] = 'X'; // Backtracking step
+            }
         }
     }
     
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // Vertical Up
+        for (int i = row-1; i>=0; i--) {
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // Diagonal Left
+        for(int i= row-1, j = col-1; i>=0 && j>=0; i--, j--) {
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        // Diagonal Right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void printChessboard(char[][] board) {
         System.out.println("---------Chessboard---------");
         for (int i = 0; i < board.length; i++) {
@@ -27,12 +50,12 @@ public class NQueens {
     }
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         // Initialize Chessboard
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
+                board[i][j] = 'X';
             }
         }
 
