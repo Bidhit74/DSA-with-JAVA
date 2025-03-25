459fe313efcf6d4984f369175b8e154ced688c35
Bidhit <bidhitkumar61@gmail.com>
2025-03-25 21:51:19 +0530

    Find maximum in an ArrayList

---
+++ ArrayList/FindMaximum.java
@@ -0,0 +1,29 @@
+// Find maximum in an ArrayList
+// input : list = 2, 5, 25, 20, 15;
+// output : 25
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindMaximum {
+
+    public static int findMaximum(ArrayList<Integer> list) {
+        if (list.size() == 0)
+            return 0;
+        int max = Integer.MIN_VALUE;
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(2, 5, 25, 20, 15));
+        
+        int max = findMaximum(list);
+        System.out.println("Maximum found: " + max);
+    }
+}
