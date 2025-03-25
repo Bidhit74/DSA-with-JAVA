d2ace442d45ea8da668daf097f72ad2935c031b3
Bidhit <bidhitkumar61@gmail.com>
2025-03-25 21:54:45 +0530

    add time complexity and efficient code

--- ArrayList/FindMaximum.java
+++ ArrayList/FindMaximum.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 public class FindMaximum {
 
+    // Time Complexity : O(n)
     public static int findMaximum(ArrayList<Integer> list) {
         if (list.size() == 0)
             return 0;
         int max = Integer.MIN_VALUE;
         int n = list.size();
         for (int i = 0; i < n; i++) {
-            if (max < list.get(i)) {
-                max = list.get(i);
-            }
+            // if (max < list.get(i)) {
+            //     max = list.get(i);
+            // }
+            // or
+            max = Math.max(max, list.get(i)); // This is more efficient than the above line.
         }
         return max;
     }
