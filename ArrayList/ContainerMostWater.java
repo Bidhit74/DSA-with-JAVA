375aa7ddd0efa03c18060935836590d0c1273aaa
Bidhit <bidhitkumar61@gmail.com>
2025-03-26 19:14:09 +0530

    Optimised Solution, Time Complexity : O(n), 2 Pointer Approach

--- ArrayList/ContainerMostWater.java
+++ ArrayList/ContainerMostWater.java
@@ -22,18 +22,33 @@ public class ContainerMostWater {
         return maxHeight;
     }
 
-    
+    // Optimised Solution
+    // Time Complexity : O(n)
+    // 2 Pointer Approach
+    public static int storeWater(ArrayList<Integer> height) {
+        int lp = 0, rp = height.size() - 1;
+        int maxWater = 0;
+        while (lp < rp) {
+            int wi = rp - lp;
+            int hi = Math.min(height.get(lp), height.get(rp));
+            int currWater = hi * wi;
+            maxWater = Math.max(maxWater, currWater);
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+    }
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
-        height.add(8);
-        height.add(6);
-        height.add(2);
-        height.add(5);
-        height.add(4);
-        height.add(5);
         height.add(3);
         height.add(5);
+        height.add(4);
         System.out.println("Maximum area: " + maxAreaBruteForce(height));
+        System.out.println("Maximum area: " + storeWater(height));
+        
     }
 }
