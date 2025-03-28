d05d5302dc72569b30f12d229ce4a64167bd4851
Bidhit <bidhitkumar61@gmail.com>
2025-03-28 18:19:12 +0530

    print Linked List

--- Linked List/LinkedListIntro.java
+++ Linked List/LinkedListIntro.java
@@ -12,29 +12,55 @@ public class LinkedListIntro {
 
     public static Node head;
     public static Node tail;
-
     // Linked list access karne ke liye Methods banayenge
-    // Add(), remove(), print(), search()
-
     // Time complexity: O(1)
     public void addFirst(int data) {
         // Step 1: Create a new node
         Node newNode = new Node(data);
         if (head == null) {
-            // head = newNode;
-            // tail = newNode; // If list is empty, both head and tail are new node
-            // or
             head = tail = newNode;
+            return;
         }
         // Step 2: Update the new node's next pointer to the current head
         newNode.next = head; // Link
         // Step 3: head is now the new node
         head = newNode;
-
     }
+
+    // Time complexity: O(1)
+    public void addLast(int data) {
+        // Step 1: Create a new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // Step 2: Update the tail node's next pointer to the new node
+        tail.next = newNode; // Link
+        // Step 3: Update the tail to the new node
+        tail = newNode;
+    }
+
+    // 
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedListIntro l1 = new LinkedListIntro();
+        l1.printLinkedList();
         l1.addFirst(1);
+        l1.printLinkedList();
         l1.addFirst(2);
+        l1.printLinkedList();
+        l1.addLast(3);
+        l1.printLinkedList();
+        l1.addLast(4);
+        l1.printLinkedList(); 
     }
 }
