f49e629183074c9f739ed4e6a90af039ba380bc7
Bidhit <bidhitkumar61@gmail.com>
2025-04-15 19:19:19 +0530

    Detect a Loop/Cycle in a Linked List

--- Linked List/Linked-List-2/CycleLinkedList.java
+++ Linked List/Linked-List-2/CycleLinkedList.java
@@ -1,8 +1,6 @@
 // Detect a Loop/Cycle in a Linked List
-
 // Floyd's cycle finding algorithm
 // slow-fast algorithm --> two pointer approach
-
 // Moves       --->   Distance
 // 0           --->   0
 // 1           --->   1
@@ -25,15 +23,6 @@ public class CycleLinkedList {
     public static Node head;
     public static Node tail;
 
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = tail = newNode;
-            return;
-        }
-        tail.next = newNode;
-        tail = newNode;
-    }
     // Detect Cycle/loop in a ll.
     // This is Floyd’s Cycle Detection Algorithm (also called the Tortoise and Hare algorithm).
     // Time Complexity : O(n)
@@ -52,14 +41,48 @@ public class CycleLinkedList {
         // cycle does not exist
         return false;
     }
+
+    // Remove a Loop/Cycle in a LL.
+    public static void removeCycle(){
+        // detect the cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+                if(slow==fast){
+                    cycle = true;
+                    break;
+                }
+        }
+        if(!cycle) return;
+
+        // find meeting point
+        slow = head;
+        Node prev = null; // last node
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //remove cycle --> last.next = null
+        prev.next = null;
+    }
+    
+
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        // 1 --> 2 --> 3 --> 1
+        head.next.next.next = temp;
+        // 1 --> 2 --> 3 --> 2
 
         System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+        
 
     }
 }
