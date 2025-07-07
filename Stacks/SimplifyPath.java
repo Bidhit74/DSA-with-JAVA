533208526982bc7ec3b7eca9e98faf11b21c18d1
Bidhit <bidhitkumar61@gmail.com>
2025-07-07 21:26:49 +0530

    Implement Simplify Path functionality with initial class structure

---
+++ Stacks/SimplifyPath.java
@@ -0,0 +1,14 @@
+//  We hava an absolute path for a file (Unix-style), simplify it. Note that absolute path always begin with ‘/’ ( root directory ), a dot in path represent current directory and double dot represents parent directory.
+
+//  Sample Input 1 : /apnacollege/
+//  Sample Output 1 : /apnacollege
+
+//  Sample Input 2 : /a/..
+//  Sample Output 2 : /
+
+
+public class SimplifyPath {
+    public static void main(String[] args) {
+        
+    }
+}
